7e9d0cd416a9c00507307255815504365b6445cf
Janse-1 <chaplezjecl03@gmail.com>
2025-05-08 22:47:28 -0500

    Superuser y vista de administrador de django

--- academico/admin.py
+++ academico/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import (
+    Usuario, Sede, Coordinador, Docente, Estudiante,
+    Padre, Grupo, Grado, Asignatura, Evaluacion, Calificacion,
+    Asistencia, AsistenciaEstudiante, DocenteSede,
+    AsignaturaDocenteGrupo, PadreEstudiante, EstudianteAsignaturaCursoGrado,
+    EvaluacionAspecto, PeriodoAcademico
+)
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Sede)
+admin.site.register(Coordinador)
+admin.site.register(Docente)
+admin.site.register(Estudiante)
+admin.site.register(Padre)
+admin.site.register(Grupo)
+admin.site.register(Grado)
+admin.site.register(Asignatura)
+admin.site.register(Evaluacion)
+admin.site.register(Calificacion)
+admin.site.register(Asistencia)
+admin.site.register(AsistenciaEstudiante)
+admin.site.register(DocenteSede)
+admin.site.register(AsignaturaDocenteGrupo)
+admin.site.register(PadreEstudiante)
+admin.site.register(EstudianteAsignaturaCursoGrado)
+admin.site.register(EvaluacionAspecto)
+admin.site.register(PeriodoAcademico)
