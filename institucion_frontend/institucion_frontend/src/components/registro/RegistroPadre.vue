4f29a75d9ba10cc63aa1e545dce3dbbb2290253d
Janse-1 <chaplezjecl03@gmail.com>
2025-05-15 00:17:27 -0500

    Registro de padres de familia

--- institucion_frontend/institucion_frontend/src/components/registro/RegistroPadre.vue
+++ institucion_frontend/institucion_frontend/src/components/registro/RegistroPadre.vue
@@ -1,18 +1,32 @@
 <script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
 const props = defineProps(['modelValue'])
 const emit = defineEmits(['update:modelValue'])
-
 const form = props.modelValue
 
 const updateField = (key, value) => {
-  emit('update:modelValue', { ...form, [key]: value })
+  form[key] = value
 }
+
 </script>
 
 <template>
   <div>
     <h3>Información del padre de familia</h3>
-    <input :value="form.telefono" @input="updateField('telefono', $event.target.value)" placeholder="Teléfono" />
-    <input :value="form.direccion" @input="updateField('direccion', $event.target.value)" placeholder="Dirección" />
+    <label>Número de telefono</label><br>
+    <input
+      :value="form.telefono"
+      @input="updateField('telefono', $event.target.value)"
+      placeholder="Teléfono"
+    /> <br>
+
+    <label>Dirección</label><br>
+    <input
+      :value="form.direccion"
+      @input="updateField('direccion', $event.target.value)"
+      placeholder="Dirección"
+    /> <br>
   </div>
 </template>
