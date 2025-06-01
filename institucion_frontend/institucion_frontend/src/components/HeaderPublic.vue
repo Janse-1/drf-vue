c95c15b245071df63f76a46aeaf3166e72f8b720
Janse-1 <chaplezjecl03@gmail.com>
2025-05-31 23:25:44 -0500

    Menú desplegable en la sección de Quienes somos

--- institucion_frontend/institucion_frontend/src/components/HeaderPublic.vue
+++ institucion_frontend/institucion_frontend/src/components/HeaderPublic.vue
@@ -10,10 +10,17 @@
         <a v-if="!isHomeView" href="/" title="Inicio">
           <i class="fa-solid fa-house"></i> Inicio
         </a>
-        <a  href="#" title="¿Quiénes somos?">
-          <i class="fa-solid fa-users"></i> Quiénes somos
-        </a>
-        <a  href="#" title="Noticias">
+        <div class="dropdown" @mouseenter="mostrarDropdown = true" @mouseleave="mostrarDropdown = false">
+          <a href="#" title="¿Quiénes somos?" class="dropdown-toggle">
+            <i class="fa-solid fa-users"></i> Quiénes somos <i class="fa-solid fa-caret-down"></i>
+          </a>
+          <div v-if="mostrarDropdown" class="dropdown-menu">
+            <a href="#" class="dropdown-item">Misión y Visión</a>
+            <a href="#" class="dropdown-item">Símbolos</a>
+            <a href="#" class="dropdown-item">Reportes</a>
+          </div>
+        </div>
+        <a href="#" title="Noticias">
           <i class="fa-solid fa-newspaper"></i> Noticias
         </a>
         <a v-if="!isContactoView" href="ContactoView" title="Contacto">
@@ -28,10 +35,13 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 
-// Ocultar "Iniciar sesión" si ya estás en la vista Login
+const mostrarDropdown = ref(false)
+
+// Ocultar La seccion si ya estás en la vista 
 const isLoginView = route.path === '/LoginView'
 const isHomeView = route.path === '/HomeView'
 const isQuienesSomos = route.path === '/QuienesSomosView'
@@ -92,5 +102,45 @@ const isContactoView = route.path === '/ContactoView'
 .navbar a:hover {
   background-color: #c31f2d;
   border-radius: 5px;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.dropdown-toggle {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 6px;
+}
+.dropdown-menu {
+  position: absolute;
+  top: 110%;
+  left: 0;
+  background: #D72638;;
+  color: #ffff;
+  min-width: 180px;
+  border-radius: 8px;
+  box-shadow: 0 4px 16px rgba(27,27,30,0.12);
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem 0;
+}
+.dropdown-item {
+  color: #D72638;
+  padding: 0.7rem 1.2rem;
+  text-decoration: none;
+  font-weight: 500;
+  transition: background 0.2s, color 0.2s;
+  border: none;
+  background: none;
+  text-align: left;
+  cursor: pointer;
+}
+.dropdown-item:hover {
+  background: #FFBA08;
+  color: #fff;
 }
 </style>
